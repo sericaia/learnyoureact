a3cb44df792f4aaf14ce828d9b4fe6bfbf783390
Daniela Borges <alunassertiva@gmail.com>
2015-06-02 18:07:52 +0100

    workshop finished

--- program.js
+++ program.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var React = require('react'); 
+var DOM = React.DOM; 
+var body = DOM.body; 
+var div = DOM.div; 
+var script = DOM.script;
+
+var browserify = require('browserify');
+
+require('node-jsx').install(); 
+
+var TodoBox = require('./views/index.jsx');
+
 var app = express();
 
 app.set('port', (process.argv[2] || 3000));
@@ -13,8 +25,34 @@ var data = [];
 data.push({title: "Shopping", detail: process.argv[3]});
 data.push({title: "Hair cut", detail: process.argv[4]});
 
-app.use('/', function(req, res) {
-    res.render('index',  {data: data});
+// app.use('/', function(req, res) {
+//     res.render('index',  {data: data});
+// });
+app.use('/bundle.js', function(req, res) { 
+  res.setHeader('content-type', 'application/javascript'); 
+  browserify('./app.js') 
+  .transform('reactify') 
+  .bundle() 
+  .pipe(res); 
 });
+
+app.use('/', function(req, res) { 
+  var initialData = JSON.stringify(data); 
+  var markup = React.renderToString(React.createElement(TodoBox, {data: data}));
+  
+  res.setHeader('Content-Type', 'text/html'); 
+  
+  var html = React.renderToStaticMarkup(body(null, 
+      div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}), 
+      script({id: 'initial-data', 
+          type: 'text/plain', 
+          'data-json': initialData 
+      }), 
+      script({src: '/bundle.js'}) 
+      )); 
+  
+  res.end(html); 
+});
+
 
 app.listen(app.get('port'), function() {});
--- views/index.jsx
+++ views/index.jsx
@@ -13,35 +13,84 @@ var TodoBox = React.createClass({
 });
 
 var TodoList = React.createClass({
+  getInitialState: function() { 
+    return { 
+      data: this.props.data, 
+      titleValue: "", 
+      detailValue: "" 
+    }; 
+  }, 
+  changeTitle: function(e) { 
+    this.setState({titleValue: e.target.value});
+  }, 
+  changeDetail: function(e) {         
+    this.setState({detailValue: e.target.value}); 
+  }, 
+  addTodo: function() { 
+    this.state.data.push({
+      title: this.state.titleValue,
+      detail: this.state.detailValue
+    });
+
+    this.setState({data: this.state.data});
+
+    /* reset values */
+    this.setState({titleValue: ""});
+    this.setState({detailValue: "" });
+  }, 
   render: function() {
     var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
     return (
       <div className="todoList">
+      <div> 
+      Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} /> 
+      Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} /> 
+      <button onClick={this.addTodo}>Add</button> 
+      </div> 
       <table style={style.tableStyle}>
       <tbody>
       { /* <Todo title="Shopping">Milk</Todo>
-       <Todo title="Hair cut">13:00</Todo> */ }
-      {todo}
-      </tbody>
-      </table>
-      </div>
-      );
+    <Todo title="Hair cut">13:00</Todo> */ }
+    {todo}
+    </tbody>
+    </table>
+    </div>
+    );
   }
 });
 
 var Todo = React.createClass({
  propTypes: {
-  title: React.PropTypes.number.isRequired
+  title: React.PropTypes.number.isRequired,
+  onDelete: React.PropTypes.func.isRequired 
 },
 getInitialState: function(){
-  return {checked: false};
+  return {
+    checked: false,
+    vodooTrStyle: style.notCheckedTodo
+  };
 }, 
 handleChange: function(){
   this.setState({checked: !checked});
+
+  if(checked){
+    this.setState({
+      vodooTrStyle: style.checkedTodo
+    });
+  } 
+  else {
+    this.setState({
+      vodooTrStyle: style.notCheckedTodo
+    });
+  }
 }, 
+_onDelete: function () { 
+  this.props.onDelete(this.props.title); 
+},
 render: function() {
   return (
-    <tr>
+    <tr style={this.state.vodooTrStyle} >
+    <td style={style.tableContent}><button onClick={this._onDelete}>X</button></td> 
     <td style={style.tableContent}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
     <td style={style.tableContent}>{this.props.title}</td>
     <td style={style.tableContent}>{this.props.children}</td>
@@ -62,12 +111,18 @@ var TodoForm = React.createClass({
 });
 
 var style = {
-      tableContent: {
-        border: "1px solid black"
-      },
-      tableStyle: {
-        border: "2px solid black"
-      }
+  tableContent: {
+    border: "1px solid black"
+  },
+  tableStyle: {
+    border: "2px solid black"
+  },
+  checkedTodo: { 
+    textDecoration: "line-through" 
+  }, 
+  notCheckedTodo: { 
+    textDecoration: "none" 
+  }
 };
 
 
